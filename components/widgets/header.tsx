3fad06181516ed6a1f1ab9ac1387d07cb5d43cab
fedega15 <federico.galassi9@gmail.com>
2024-01-25 12:38:37 -0300

    ASD

--- components/widgets/header.tsx
+++ components/widgets/header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   };
 
   return (
-    <header className="fixed w-full left-0 top-0 z-50">
+    <header className="fixed w-full left-0 -top-2 z-50">
       <div className="container mx-auto flex items-center justify-between p-5 lg:py-5 lg:px-0">
         <section>
           <Logo />
@@ -19,14 +19,23 @@ export const Header = () => {
         <section>
           <Nav />
           <button className="relative lg:hidden" onClick={toggleMenu}>
-            <span className="absolute right-0 -top-4 w-8 h-0.5 bg-white"></span>
-            <span className="absolute right-0 -top-2 w-8 h-0.5 bg-white"></span>
-            <span className="absolute right-0 -top-0 w-8 h-0.5 bg-white"></span>
+            {isMenuOpen ? (
+            <div className="flex justify-center items-center">
+            <span className="right-1 -top-2 block absolute w-6 h-0.5 bg-white transform rotate-45"></span>
+            <span className="right-1 -top-2 block absolute w-6 h-0.5 bg-white transform -rotate-45"></span>
+          </div>
+            ) : (
+              <>
+                <span className="absolute right-0 -top-4 w-8 h-0.5 bg-white"></span>
+                <span className="absolute right-0 -top-2 w-8 h-0.5 bg-white"></span>
+                <span className="absolute right-0 -top-0 w-8 h-0.5 bg-white"></span>
+              </>
+            )}
           </button>
         </section>
       </div>
       {isMenuOpen && (
-        <div className="absolute top-[55px] right-0 backdrop-blur-lg bg-white bg-opacity-25 w-[120px] max-w-xs lg:hidden">
+        <div className="absolute top-[55px] right-0 backdrop-blur-lg bg-white bg-opacity-25 w-[116px] max-w-xs lg:hidden">
           <ul className="text-white">
             <li className="text-lg hover:text-gray-300 p-2 pt-0 ">
               <a href="/">INICIO</a>
