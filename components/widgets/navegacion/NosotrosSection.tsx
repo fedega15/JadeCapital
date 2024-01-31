84cce938a703e4c884a55bedbafd3084ef9fe2f8
fedega15 <federico.galassi9@gmail.com>
2024-01-31 14:20:19 -0300

    ASD

--- components/widgets/navegacion/NosotrosSection.tsx
+++ components/widgets/navegacion/NosotrosSection.tsx
@@ -9,7 +9,7 @@ const NosotrosSection = () => {
       <br />
       <div  className="container px-5 py-2 mx-auto">
         <div  className="flex mt-6 justify-center">
-          <div className="w-[700px] h-[2px] rounded-full mt-[-30px] bg-yellow-500 inline-flex"></div>
+          <div className="w-[100%] h-[0.08px] rounded-full mt-[-30px] bg-yellow-500 inline-flex"></div>
         </div>
         <div className="text-center mb-20">
           <div className="w-[222px] h-[100px] inline-flex items-center justify-centermb-5">
@@ -27,22 +27,14 @@ const NosotrosSection = () => {
           <br />
           <br />
           <br />
-
-          <p className=" mb-[50px] text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-white">
-            Somos un equipo de profesionales especializados en la organización
-            de eventos exclusivos. Además, y sobre todas las cosas, somos
-            artífices de grandes sueños, un grupo humano que entiende que vale
-            la pena celebrar la vida y ofrece un escenario para que todos los
-            anhelos se conviertan en realidad. Desde 2009 hasta la actualidad
-            hemos hecho posible más de 800 experiencias únicas que perdurarán
-            para siempre en la memoria de quienes nos elijen. Evolucionamos en
-            nuestras propuestas y servicios, siempre desde un espacio
-            privilegiado tanto para reuniones sociales como corporativas.
-          </p>
+          <p className="mb-[50px] text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-white sm:text-justify" style={{ textAlign: "justify", textJustify: "inter-word", hyphens: "auto" }}>
+    Somos un equipo de profesionales especializados en la organización de eventos exclusivos. Además, y sobre todas las cosas, somos artífices de grandes sueños, un grupo humano que entiende que vale la pena celebrar la vida y ofrece un escenario para que todos los anhelos se conviertan en realidad. Desde 2009 hasta la actualidad hemos hecho posible más de 800 experiencias únicas que perdurarán para siempre en la memoria de quienes nos elijen. Evolucionamos en nuestras propuestas y servicios, siempre desde un espacio privilegiado tanto para reuniones sociales como corporativas.
+  </p>
+  
           <br id="servicios" />
           <br />
           <div className="flex mt-[36px] justify-center">
-            <div className="w-[700px] h-[2px] -mb-[100px] -mt-2 rounded-full bg-yellow-500 inline-flex"></div>
+            <div className="w-[100%] h-[0.08px] -mb-[100px] -mt-2 rounded-full bg-yellow-500 inline-flex"></div>
           </div>
           <br />
           <br />
@@ -71,12 +63,12 @@ const NosotrosSection = () => {
           )}
           {renderCard(
             "Equipamiento audiovisual",
-            "Transformando Eventos en Experiencias Audiovisuales Inolvidables. En Jade Fiestas, nos especializamos en llevar la magia del sonido y la imagen a cada rincón de tus eventos. Con una pasión inigualable por la perfección audiovisual, nuestro equipo ofrece soluciones integrales para hacer de cada celebración o reunión profesional un espectáculo sensorial único.",
+            "Transformando eventos en experiencias audiovisuales inolvidables. En Jade Fiestas, nos especializamos en llevar la magia del sonido y la imagen a cada rincón de tus eventos. Con una pasión inigualable por la perfección audiovisual, nuestro equipo ofrece soluciones integrales para hacer de cada celebración o reunión profesional un espectáculo sensorial único.",
             "/imgs/c.jpg"
           )}
         </div>
         <div className="flex mt-6 justify-center">
-          <div className="w-[700px] h-[2px] rounded-full bg-yellow-500 inline-flex"></div>
+          <div className="w-[100%] h-[0.08px] rounded-full bg-yellow-500 inline-flex"></div>
         </div>
         <div
         className="fixed left-4 bottom-4 flex items-center cursor-pointer z-6"
@@ -110,12 +102,13 @@ const renderCard = (title: string, description: string, image: string) => (
         />
       </div>
     </div>
-    <div className="flex-grow">
-      <h2 className="text-yellow-500 text-lg title-font font-medium mb-3">
-        {title}
-      </h2>
-      <p className="leading-relaxed text-white">{description}</p>
-    </div>
+    <p className="mb-[50px] text-base leading-relaxed xl:w-4/4 lg:w-4/4 mx-auto text-white sm:text-justify" style={{ textAlign: "justify", letterSpacing: "-0.08em", hyphens: "auto" }}>
+  {description}
+</p>
+
+
+
+
   </div>
 );
 
